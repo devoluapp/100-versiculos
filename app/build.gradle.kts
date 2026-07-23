3592c6f9775db01be1f8ff8016f33daa3e6713d3
Roberto Tavares <falecomroberto@gmail.com>
2026-07-23 12:41:27 -0300

    Bump de versão 1.10 -> 1.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "blog.robertotavares.cemversiculos"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.10"
+        versionCode = 11
+        versionName = "1.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
